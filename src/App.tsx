1d1e5e2c68bacc14f04eae21eb84b60c143d7357
Derick-Almeida <d.a.s2000silva14@gmail.com>
2024-04-10 18:36:07 -0300

    pagina de pagamento

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import WidgetWhatsapp from "./components/WidgetWhatsapp";
 import PopupDiscount from "./components/PopupDiscount";
 import WidgetPrivacity from "./components/WidgetPrivacity";
 import MainTransition from "./components/MainTransition";
+import Payment from "./components/Payment";
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header />
       <div className="js-tail-topo-ancora"></div>
       <MainTransition />
       <Footer />
 
       <WidgetWhatsapp numero="5548991729419" />
       <PopupDiscount />
-      <WidgetPrivacity />
+      <WidgetPrivacity /> */}
+
+      <Payment />
     </>
   );
 }
