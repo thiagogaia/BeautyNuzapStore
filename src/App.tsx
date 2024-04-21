765b62c5dc0651f6b9ad456d1d883b2d03e67f11
thigato <thiagogaia@gmail.com>
2024-04-21 15:47:01 -0300

    implements fingerprintsync in header requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import "./App.css";
 
 import Routes from "./routes";
-import FingerprintJS from "@fingerprintjs/fingerprintjs";
-
-async function computeHash() {
-  const fpPromise = FingerprintJS.load({ monitoring: false });
-  const fp = await fpPromise;
-  const result = await fp.get();
-  window.localStorage.setItem("userFingerPrint", result.visitorId);
-  return result;
-}
-
 function App() {
-  computeHash();
-
   return (
     <>
       <div className="js-tail-topo-ancora"></div>
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,18 @@
 import axios from "axios";
+import { getCookies } from "./cookies";
+// @ts-ignore
+import Fingerprint2 from "fingerprintjs2sync";
 
+
+const fprint = (new Fingerprint2()).getSync().fprint;
 const BASE_URL = import.meta.env.VITE_API_URL
 
 export const api = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
+  headers: {   
+    "Content-Type": "application/json", 
+    "Fingerprint": fprint,
+    "Cookies": JSON.stringify(getCookies())
+  }
 });
