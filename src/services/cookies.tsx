d292587a8721d4e1ec8896ca5a575a264a7f2d24
Derick-Almeida <d.a.s2000silva14@gmail.com>
2024-04-22 16:27:05 -0300

    trocando `var` por `let` e `const`

--- src/services/cookies.tsx
+++ src/services/cookies.tsx
@@ -1,20 +1,20 @@
 export function getCookies(): { [key: string]: string } {
-  var pairs = document.cookie.split(";").filter(n => n)
-  var cookies: { [key: string]: string } = {}
-  for (var i = 0; i < pairs.length; i++) {
-    var pair = pairs[i].split("=");
-    cookies[(pair[0] + '').trim()] = unescape(pair.slice(1).join('='));
+  const pairs = document.cookie.split(";").filter((n) => n);
+  const cookies: { [key: string]: string } = {};
+  for (let i = 0; i < pairs.length; i++) {
+    const pair = pairs[i].split("=");
+    cookies[(pair[0] + "").trim()] = unescape(pair.slice(1).join("="));
   }
   return cookies;
 }
 
 export function setCookie(nome: string, valor: string, durationDias = 15): void {
-  var duracao = new Date()
-  duracao.setDate(duracao.getDate() + durationDias)
-  var dominioPrincipal = /(?<=\.).+/.exec(location.hostname)
-  var cookiePeriodo = nome + "=" + encodeURIComponent(valor)
-  cookiePeriodo += (duracao ? "; expires=" + duracao.toUTCString() : "")
-  if (dominioPrincipal) cookiePeriodo += "; domain=" + dominioPrincipal[0]
-  cookiePeriodo += ';path=/;'
+  const duracao = new Date();
+  duracao.setDate(duracao.getDate() + durationDias);
+  const dominioPrincipal = /(?<=\.).+/.exec(location.hostname);
+  let cookiePeriodo = nome + "=" + encodeURIComponent(valor);
+  cookiePeriodo += duracao ? "; expires=" + duracao.toUTCString() : "";
+  if (dominioPrincipal) cookiePeriodo += "; domain=" + dominioPrincipal[0];
+  cookiePeriodo += ";path=/;";
   document.cookie = cookiePeriodo;
 }
